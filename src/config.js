66892bd6909611d6f600b470362db4167f005611
Rigoberto Acifuina <racifuina@me.com>
2022-10-07 19:06:57 -0600

    tercera entrega

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export const API_SERVER_ENDPOINT_BASE = process.env.REACT_APP_API_SERVER_ENDPOINT_BASE;
-// export const API_SERVER_ENDPOINT_BASE = 'http://192.168.1.19:4000';
 export const AUTH_TOKEN_KEY_NAME = process.env.REACT_APP_AUTH_TOKEN_KEY_NAME;
 export const REDUX_STORE_NAME = process.env.REACT_APP_REDUX_STORE_NAME;
 export const Endpoint = endpoint => API_SERVER_ENDPOINT_BASE + endpoint;
